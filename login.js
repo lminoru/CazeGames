f9cff6cb8d0d4aa292f599d48cacee6d1ec6f017
Leonardo Goncalves Chahud <lchahud02@usp.br>
2022-12-07 19:52:42 -0300

    VLibras

---
+++ login.js
@@ -0,0 +1,13 @@
+
+
+function do_login(){
+	document.cookie="authenticated=true";
+}
+
+function do_register() {
+	document.cookie="authenticated=true";
+}
+
+function redirect_if_authenticated() {
+	
+}
