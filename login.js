1d150da71ecc89398bb28f7de50721771c290b8c
lchahud <leonardo.chahud.dev@gmail.com>
2022-12-08 10:15:56 -0300

    modified:   carrinho_compras.css 	modified:   carrinho_compras.html 	modified:   confirmacao_compra.html 	modified:   index.css 	modified:   index.html 	modified:   info_produto.css 	modified:   info_produto.html 	modified:   login.css 	modified:   login.html 	modified:   login.js 	modified:   register.html

--- login.js
+++ login.js
@@ -1,13 +1,41 @@
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 function do_login(){
-	document.cookie="authenticated=true";
+	document.cookie='authenticated=true';
 }
 
 function do_register() {
-	document.cookie="authenticated=true";
+	document.cookie='authenticated=true';
 }
 
-function redirect_if_authenticated() {
-	
+function do_exit(){
+	document.cookie='authenticated=false';
 }
+
+function is_logged() {
+	if (getCookie('authenticated') === 'true'){
+		return true;
+	}
+	return false;
+}
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
